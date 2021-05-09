c02f7ee90ed4c16976c4b217ac0e7daf57337f0c
Petrachkov A.D <xoxoji19910501@gmail.com>
2021-05-09 16:51:22 +0300

    feat: copy input file to output file

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const argv = require("./argv");
+const fs = require("fs");
+const { pipeline } = require("stream");
 
 const options = argv.getArgvOptions(process.argv);
+
+pipeline(
+    fs.createReadStream(options.input),
+    fs.createWriteStream(options.output),
+    (err) => {
+        if (err) {
+            console.error("Pipeline failed.", err);
+        }
+    }
+);
 
 console.log(options);
