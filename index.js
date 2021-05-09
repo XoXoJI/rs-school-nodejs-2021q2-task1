f005bc4077122e004275725eccc4c53bc15e3901
Petrachkov A.D <xoxoji19910501@gmail.com>
2021-05-09 19:04:04 +0300

    feat: add encode

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const argv = require("./argv");
 const fs = require("fs");
-const { pipeline } = require("stream");
+const { pipeline, Transform } = require("stream");
+const encode = require("./encode");
 
 const options = argv.getArgvOptions(process.argv);
+const shift = options.isEncode ? options.shift : -options.shift;
+
+const encodeTransformStream = new Transform({
+    transform(chunk, encoding, callback) {
+        const data = encode(chunk.toString(), shift);
+
+        callback(null, data);
+    },
+});
 
 pipeline(
     fs.createReadStream(options.input),
+    encodeTransformStream,
     fs.createWriteStream(
         options.output,
         {
